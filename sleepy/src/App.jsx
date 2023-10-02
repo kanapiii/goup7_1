de3eaf679c370b60bb0f88cffbddb2095f064fe8
kanapiii <113953054+kanapiii@users.noreply.github.com>
2023-10-02 12:19:54 +0900

    環境構築
    reactの環境構築を実施

---
+++ sleepy/src/App.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import liff from "@line/liff";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    liff
+      .init({
+        liffId: import.meta.env.VITE_LIFF_ID
+      })
+      .then(() => {
+        setMessage("LIFF init succeeded.");
+      })
+      .catch((e) => {
+        setMessage("LIFF init failed.");
+        setError(`${e}`);
+      });
+  });
+
+  return (
+    <div className="App">
+      <h1>group7チーム</h1>
+    </div>
+  );
+}
+
+export default App;
