280b4f0b22aea6965bc6437e9b16f8f5ac40487c
shooooooogo <shogo180520@icloud.com>
2023-10-30 11:23:54 +0900

    ぷるりく

--- sleepy/src/Components/Board.jsx
+++ sleepy/src/Components/Board.jsx
@@ -1,12 +1,21 @@
 import "../Style/Board.css";
 
 
+
 function Board(){
-const apple="アップル";
+const name="名前表示";
   return (
     <div className="Board">
       <h1>掲示板</h1>
-      <p className="p1">{apple}</p>l
+      <div className="header">
+        <div class="circle-image-container">
+          <img src="path_to_your_image.jpg" alt="アイコン表示アイコン表示アイコン表示アイコン表示"/>
+        </div>
+        <span className="name">{name}</span>
+        <span className="time">10:14</span>
+        <span className="title">sleepy</span>
+        {/* 他のヘッダーアイコンや要素 */}
+      </div>
     </div>
   );
 
