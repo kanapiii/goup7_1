fe15c759ba5f0a577c55723e0bd8354fa99fb528
shooooooogo <shogo180520@icloud.com>
2023-11-17 11:21:00 +0900

    comiit

--- sleepy/src/App.jsx
+++ sleepy/src/App.jsx
@@ -9,6 +9,7 @@ import Diary from "./Components/Diary";
 import Graph from "./Components/Graph";
 import Morning from "./Components/Morning";
 import Night from "./Components/Night";
+import AllDiary from "./Components/AllDiary";
 
 function App() {
   // const [message, setMessage] = useState("");
@@ -38,6 +39,7 @@ function App() {
         <Route path={`/Graph`} element={<Graph />}/>
         <Route path={`/Morning`} element={<Morning />}/>
         <Route path={`/Night`} element={<Night />}/>
+        <Route path={`/AllDiary`} element={<AllDiary />}/>
       </Routes>
     </BrowserRouter>
   );
