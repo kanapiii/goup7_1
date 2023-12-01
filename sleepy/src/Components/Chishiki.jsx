8a980a7434b32d5a17ea90e7a74c412bdf85ce1b
Ryuking2525 <ryuuu2234@gmail.com>
2023-12-01 11:26:42 +0900

    こうしん

--- sleepy/src/Components/Chishiki.jsx
+++ sleepy/src/Components/Chishiki.jsx
@@ -1,31 +1,21 @@
+import React from "react";
 import "../Style/Chishiki.css";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
 
 function Chishiki() {
   return (
     <div className="Chishiki">
       <h1>睡眠に関する知識</h1>
       <div className="box">
-        <Box
-          component="form"
-          sx={{
-            "& .MuiTextField-root": { m: 1, width: "70ch"}
-          }}
-          noValidate
-          autoComplete="off"
-        >
+        <form className="custom-form" noValidate autoComplete="off">
           <div>
-            <TextField
-              id="outlined-multiline-static"
-              label=""
-              multiline
+            <input
+              id="Button"
               rows={4}
-              defaultValue="aa"
-              InputProps={{ readOnly: true }}
+              defaultValue="あああああああああああああああああああああああああああああああああああ"
+              readOnly
             />
           </div>
-        </Box>
+        </form>
       </div>
     </div>
   );
